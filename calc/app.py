01eff700db1a3ff470e03d5dd1a28ac5a6cf34ec
Jacques Pierre <pierreandassoc.gmail>
2022-01-24 20:43:19 -0700

    completed the calc app.py application

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,32 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def addition():
+	"""Perform addition operation."""
+	a = int(request.args['a'])
+	b = int(request.args['b'])
+	return str(add(a, b))
+
+@app.route('/sub')
+def subtraction():
+	"""Perform subtraction operation."""
+	a = int(request.args['a'])
+	b = int(request.args['b'])
+	return str(sub(a, b))
+
+@app.route('/mult')
+def multiplication():
+	"""Perform multiplication operation."""
+	a = int(request.args['a'])
+	b = int(request.args['b'])
+	return str(mult(a, b))
+
+@app.route('/div')
+def division():
+	"""Perform division operation."""
+	a = int(request.args['a'])
+	b = int(request.args['b'])
+	return str(div(a, b))
--- greet/app.py
+++ greet/app.py
@@ -3,16 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/welcome')
-def welcome_page():
+def welcome():
 	"""Shows Welcome message"""
 	return "welcome"
 
 @app.route('/welcome/home')
-def welcome_home_page():
+def welcome_home():
 	"""Shows welcome home message"""
 	return "welcome home"
 
 @app.route('/welcome/back')
-def welcome_back_page():
+def welcome_back():
 	"""Shows welcome back message"""
 	return "welcome back"
